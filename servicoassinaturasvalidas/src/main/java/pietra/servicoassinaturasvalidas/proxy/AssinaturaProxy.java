8610f898ccfa2ab098261df2ce33175f69b2dd66
pietra renner <pietra.renner@gmail.com>
2024-06-30 20:03:59 -0300

    mudando o feign

--- servicoassinaturasvalidas/src/main/java/pietra/servicoassinaturasvalidas/proxy/AssinaturaProxy.java
+++ servicoassinaturasvalidas/src/main/java/pietra/servicoassinaturasvalidas/proxy/AssinaturaProxy.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import pietra.servicoassinaturasvalidas.entities.Assinatura;
 
-@FeignClient(name = "servicocadastramento")
+@FeignClient(name = "servicocadastramento", url = "http://localhost:8000")
 public interface AssinaturaProxy {
     @GetMapping("/servicocadastramento/servcad/assinatura/{id}")
     public Assinatura getAssinaturaId(@PathVariable("id") Long id);
